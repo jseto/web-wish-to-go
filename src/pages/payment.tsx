d67da90f7d20daeb0ec96ee43e969f8d84155a55
Josep Seto <jsetojseto@gmail.com>
2019-12-30 13:52:25 +0100

    Subscription finished

--- src/pages/payment.tsx
+++ src/pages/payment.tsx
@@ -4,46 +4,23 @@ import { SEO } from "../components/seo"
 import * as firebase from 'firebase/app'
 import 'firebase/auth'
 
-interface SubscriptionProps {
+interface PaymentProps {
 	location: Location;
 }
 
-interface SubscriptionState {
-	email: string;
-	password: string;
+interface PaymentState {
 }
 
-export class Subscription extends React.Component<SubscriptionProps, SubscriptionState> {
+export class Payment extends React.Component<PaymentProps, PaymentState> {
 	static initialized = false;
 
-	constructor( props: SubscriptionProps ) {
+	constructor( props: PaymentProps ) {
 		super( props );
 		this.plan = location.search.slice(6);
-
-		this.state = {
-			email: '',
-			password: ''
-		}
-
-		const firebaseConfig = { 															// cspell: disable
-			apiKey: "AIzaSyAL7HGSveC9Qo_hUA-pIShdlEi3XWGI-Sg",
-			authDomain: "wish-to-go.com",
-			databaseURL: "https://wish-to-go.firebaseio.com",
-			projectId: "wish-to-go",
-			storageBucket: "wish-to-go.appspot.com",
-			messagingSenderId: "760218719658",
-			appId: "1:760218719658:web:5eddc4739e81f6fdaa87d3",
-			measurementId: "G-KYSSTEMQCG" 											// cspell: enable
-		};
-
-		if ( !Subscription.initialized ) {
-			firebase.initializeApp(firebaseConfig);
-			Subscription.initialized = true;
-		}
-	} 
+	}
 
 	render() {
-		const { email, password } = this.state;
+		// const { email, password } = this.state;
 
 		return (
 			<Layout>
@@ -51,7 +28,7 @@ export class Subscription extends React.Component<SubscriptionProps, Subscriptio
 
 				<div className="hero is-primary" style={{ textAlign: 'center' }}>
 				<div className="hero-body">
-					<h1 className="title">Subscribe to { this.plan } plan</h1>
+					<h1 className="title">Payment for { this.plan } plan</h1>
 				</div>
 				</div>
 
@@ -66,7 +43,7 @@ export class Subscription extends React.Component<SubscriptionProps, Subscriptio
 										placeholder="Email"
 										type="email"
 										onChange={ event => this.setState({ email: event.target.value })}
-										value={email}
+										// value={email}
 									/>
 									<span className="icon is-small is-left">
 										<i className="fas fa-envelope"></i>
@@ -78,7 +55,7 @@ export class Subscription extends React.Component<SubscriptionProps, Subscriptio
 										placeholder="Password"
 										type="password"
 										onChange={event => this.setState({ password: event.target.value })}
-										value={password}
+										// value={password}
 									/>
 									<span className="icon is-small is-left">
 										<i className="fas fa-lock"></i>
@@ -87,7 +64,7 @@ export class Subscription extends React.Component<SubscriptionProps, Subscriptio
 								<div className="field control is-grouped is-grouped-centered">
 									<button	
 										className="button is-success"
-										onClick={ () => this.emailSignUp() }
+										// onClick={ () => this.emailSignUp() }
 									>
 										Subscribe with Email
 									</button>
@@ -99,7 +76,7 @@ export class Subscription extends React.Component<SubscriptionProps, Subscriptio
 							<p>
 								<button 
 									className="button is-info is-fullwidth"
-									onClick={() => this.signUp( new firebase.auth.GoogleAuthProvider() ) }
+									// onClick={() => this.signUp( new firebase.auth.GoogleAuthProvider() ) }
 								>
 									<span>Subscribe with Google</span>
 									<span className="icon">
@@ -110,7 +87,7 @@ export class Subscription extends React.Component<SubscriptionProps, Subscriptio
 							<p>
 								<button 
 									className="button is-info is-fullwidth"
-									onClick={() => this.signUp( new firebase.auth.FacebookAuthProvider() )}
+									// onClick={() => this.signUp( new firebase.auth.FacebookAuthProvider() )}
 								>
 									<span>Subscribe with Facebook</span>
 									<span className="icon">
@@ -126,33 +103,7 @@ export class Subscription extends React.Component<SubscriptionProps, Subscriptio
 		)
 	}
 
-	private async emailSignUp() {
-		const { email, password } = this.state;
-		try {
-			const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password)	
-			await userCredential.user.sendEmailVerification({
-				url: `https://wish-to-go.com/payment?plan=${this.plan}`
-			})
-			console.log( 'sent email' );
-			window.location.href = `/verification-email-sent`
-		}
-		catch ( error ) {
-			console.error( 'Error when sign-up: ', error.code );
-		}
-	}
-
-	private async signUp( provider: firebase.auth.AuthProvider ) {
-		try {
-			const userCredential = await firebase.auth().signInWithPopup(provider)
-			console.log( userCredential.user );
-			window.location.href = `/payment?plan=${this.plan}`
-		}
-		catch( error ) {
-			console.error( 'Error when sign-up: ', error.code );
-		}
-	}
-
 	private plan: string;
 }
 
-export default Subscription
+export default Payment
---
+++ src/posts/generic/verification-email-sent.md
@@ -0,0 +1,10 @@
+---
+title: Verification Email has been Sent
+date: 2019-12-30T04:59:26Z
+category: generic
+---
+
+# A Verification Email has been Sent to your Mailbox
+
+Please check your mailbox and click the verification link sent to you
+
