0b4deb06f4556d8f3d9ec0f8784661379ea620e8
Josep Seto <jsetojseto@gmail.com>
2020-01-11 04:02:30 +0100

    installed catch-links plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,8 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
 		'gatsby-plugin-sass',
 		`gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
+		`gatsby-transformer-sharp`,
+		'gatsby-plugin-catch-links',
 		{
 	    resolve: `gatsby-transformer-remark`,
 	    options: {
